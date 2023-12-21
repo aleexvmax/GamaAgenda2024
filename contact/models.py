a56be547e34a1ff1459240ec42a286a6dbe56d70
Alex Maximiano <aleexvmax@gmail.com>
2023-12-21 13:10:56 -0300

    Config Contact Models

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,13 @@
+from email.policy import default
+from time import timezone
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Contact(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=50)
+    email = models.EmailField(max_length=200, blank=True, null=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    description = models.TextField(blank=True)
+
